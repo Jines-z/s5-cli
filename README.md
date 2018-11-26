de3e35e23966febd2e5c9150c5c5e97b2f992e2e
郑傲今 <zhengaj@shinemo.com>
2018-11-26 15:00:10 +0800

    feat: V1.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# S5-CLI
+_小需求最佳解决方案。_
+
+**技术栈**：
+*   Gulp
+*   Babel 7
+*   EsLint
+*   ES6
+*   Scss
+*   Handlebars
+*   Http-proxy
+*   指纹
+
+### Install
+```
+npm install -g s5-cli
+```
+### Init
+```
+s5 init <name>
+```
+### Help
+```
+s5 -h
+```
---
+++ packages/Download.js
@@ -0,0 +1,35 @@
+const download = require('download-git-repo')
+const chalk    = require('chalk')
+const Spinner  = require('../lib/Spinner')
+const Banner   = require('../lib/Banner')
+const Div      = require('../lib/Div')
+const Install  = require('../lib/Install')
+const Inquirer = require('../lib/Inquirer')
+const { readDir }   = require('../lib/Utils')
+
+const Download = async (dir) => {
+    await Banner('s5-cli')
+    const answers = await Inquirer()
+    process.stdout.write('\033c')
+    Spinner.start()
+    download('github:beverle-y/h5-template', `${dir}/`, async function (err) {
+        if (!err) {
+            Spinner.stop()
+            console.log(`# Download completed! \n`)
+            const fileNames = await readDir(dir)
+            Div(fileNames)
+            await Install(dir, answers.command)
+            console.log('\n')
+            console.log(`## Successfully created project ${dir}`)
+            console.log(`## Get started with the following commands:\n`)
+            console.log(` ${chalk.gray('$')} ${chalk.cyan(`cd ${dir}`)}`)
+            console.log(` ${chalk.gray('$')} ${chalk.cyan(`npm start`)}\n`)
+        } else {
+            Spinner.stop()
+            console.log(chalk.red('\n Download template failed ! \n'))
+            process.exit(0)
+        }
+    })
+}
+
+module.exports = Download
